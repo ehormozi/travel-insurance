02ceb1a26b4cf7ef666d73e33958a8de822141df
ehormozi <53533890+ehormozi@users.noreply.github.com>
2022-01-26 17:01:48 +0100

    Update Date.sol

--- contracts/Date.sol
+++ contracts/Date.sol
@@ -8,11 +8,11 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 contract Date {
 
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minute;
+    int private year;
+    int private month;
+    int private day;
+    int private hour;
+    int private minute;
 
     /**
      * @dev A date
@@ -27,6 +27,50 @@ contract Date {
         month = _month;
         day = _day;
         hour = _hour;
+        minute = _minute;
+    }
+
+    //Getters
+
+    function getYear() external view returns(int) {
+        return year;
+    }
+
+    function getMonth() external view returns(int) {
+        return month;
+    }
+
+    function getDay() external view returns(int) {
+        return day;
+    }
+
+    function getHour() external view returns(int) {
+        return hour;
+    }
+
+    function getMinute() external view returns(int) {
+        return minute;
+    }
+
+    //Setters
+
+    function setYear(int _year) external {
+        year = _year;
+    }
+
+    function setMonth(int _month) external {
+        month = _month;
+    }
+
+    function setDay(int _day) external {
+        day = _day;
+    }
+
+    function setHour(int _hour) external {
+        hour = _hour;
+    }
+
+    function setMinute(int _minute) external {
         minute = _minute;
     }
 }
