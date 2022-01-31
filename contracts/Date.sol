c838a52b02228b7802855c32ca615ca3dc409e83
ehormozi <53533890+ehormozi@users.noreply.github.com>
2022-01-31 22:42:23 +0100

    Update Date.sol

--- contracts/Date.sol
+++ contracts/Date.sol
@@ -55,22 +55,60 @@ contract Date {
     //Setters
 
     function setYear(int _year) external {
+        
+        assert (_year >= year);
+        
         year = _year;
     }
 
-    function setMonth(int _month) external {
+    function setMonth(int _month) external 
+    {
+    
+        assert( _month >= 1 && _month <= 12);
+        
         month = _month;
     }
 
-    function setDay(int _day) external {
+    function setDay(int _day) external 
+    {
+    
+        assert( _day >= 1) ;
+     
+        if ( month == 2)
+        {
+        
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                assert(_day <= 29);
+            }
+            else
+            {
+                assert(_day <= 28);
+            }
+        }
+        else if (month == 1 || month == 3 || month ==  5 || month == 7 || month == 8 || month == 10 || month == 12)
+        {
+            assert(_day <= 31);
+        }
+        else
+        {
+            assert(_day <= 30);    
+        }
+
         day = _day;
     }
 
     function setHour(int _hour) external {
+        
+        assert( _hour >= 0 && _hour <=23);
+        
         hour = _hour;
     }
 
-    function setMinute(int _minute) external {
+    function setMinute(int _minute) external 
+    {
+        assert(_minute >= 0 && _minute <= 59);
+        
         minute = _minute;
     }
 }
