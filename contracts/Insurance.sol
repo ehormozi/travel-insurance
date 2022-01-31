97e61e28d6bdac2480798b29f8cb63fbaa391137
ehormozi <53533890+ehormozi@users.noreply.github.com>
2022-01-31 22:43:00 +0100

    Update Insurance.sol

--- contracts/Insurance.sol
+++ contracts/Insurance.sol
@@ -11,30 +11,24 @@ import './User.sol';
  */
 abstract contract Insurance {
     
-    int private id;
     Date private expirationDate;
     User private user;
     Flight private flight;
+    uint256 constant eurWeiExchangeRate = 44;
 
     /**
      * @dev A flight insurance involoving one user and one flight
-     * @param _id insurance's unique identifier
      * @param _expirationDate insurance's expiration date
      * @param _user insurance's subscriber
      * @param _flight insurance's flight
      */
-    constructor(int _id, Date _expirationDate, User _user, Flight _flight) {
-        id = _id;
+    constructor(Date _expirationDate, User _user, Flight _flight) {
         expirationDate = _expirationDate;
         user = _user;
         flight = _flight;
     }
 
     //Getters
-
-    function getID() external view returns(int) {
-        return id;
-    }
 
     function getExpirationDate() external view returns(Date) {
         return expirationDate;
@@ -50,10 +44,6 @@ abstract contract Insurance {
 
     //Setters
 
-    function setID(int _id) external {
-        id = _id;
-    }
-
     function setExpirationDate(Date _expirationDate) external {
         expirationDate = _expirationDate;
     }
@@ -65,35 +55,74 @@ abstract contract Insurance {
     function setFlight(Flight _flight) external {
         flight = _flight;
     }
-    
-    //Search a fly 
-    /*
-    function SearhFly(string departureVille, int day, int year, int month)
-    {
+
+    //Functions
+    function refundUser(uint256 amount) public payable returns(bytes memory) {
+        address payable _to = (this).getUser().getAddress();
+        (bool sent, bytes memory data) = _to.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+        return data;
+    }
+}
+
+contract Classic is Insurance
+{
+    /**
+     * @dev A flight insurance involoving one user and one flight
+     * @param _expirationDate insurance's expiration date
+     * @param _user insurance's subscriber
+     * @param _flight insurance's flight
+     */
+    constructor(Date _expirationDate, User _user, Flight _flight)
+    Insurance(_expirationDate, _user, _flight) {}
+
+    function flightCanceled() public payable returns(bytes memory) {
+        return refundUser((this).getFlight().getPriceInEuros() * eurWeiExchangeRate / 100000);
+    }
+}
+
+contract Premium is Insurance
+{
+    uint256 constant delayIndemnitiyInEuros = 50;
+    /**
+     * @dev A flight insurance involoving one user and one flight
+     * @param _expirationDate insurance's expiration date
+     * @param _user insurance's subscriber
+     * @param _flight insurance's flight
+     */
+    constructor(Date _expirationDate, User _user, Flight _flight)
+    Insurance(_expirationDate, _user, _flight) {}
+
+    function flightCanceled() public payable returns(bytes memory) {
+        return refundUser((this).getFlight().getPriceInEuros() * eurWeiExchangeRate / 100000);
     }
 
-    function checkContractValidity(int monthEnd, int dayEnd, int yearEnd;)
-    {
+    function flightDelayed(uint256 nbHoursDelay) public payable returns(bytes memory) {
+        return refundUser(nbHoursDelay * delayIndemnitiyInEuros * eurWeiExchangeRate / 200000);
+    }
+}
+
+contract Golden is Insurance
+{
+    uint256 constant delayIndemnitiyInEuros = 100;
+    /**
+     * @dev A flight insurance involoving one user and one flight
+     * @param _expirationDate insurance's expiration date
+     * @param _user insurance's subscriber
+     * @param _flight insurance's flight
+     */
+    constructor(Date _expirationDate, User _user, Flight _flight)
+    Insurance(_expirationDate, _user, _flight) {}
+
+    function flightCanceled() public payable returns(bytes memory) {
+        return refundUser((this).getFlight().getPriceInEuros() * eurWeiExchangeRate / 100000);
     }
 
-    function payFly()
-    {
+    function flightDelayed(uint256 nbHoursDelay) public payable returns(bytes memory) {
+        return refundUser(nbHoursDelay * delayIndemnitiyInEuros * eurWeiExchangeRate / 100000);
     }
-    */
+
+    function flightMissed() public payable returns(bytes memory) {
+        return refundUser((this).getFlight().getPriceInEuros() * eurWeiExchangeRate / 200000);
+    }
 }
-/*
-contract Classic is  flightinsurance
-{
-
-}
-
-contract Premium is  flightinsurance
-{
-
-}
-
-contract Golden is  flightinsurance
-{
-
-}
-*/
